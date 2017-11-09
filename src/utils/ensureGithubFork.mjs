ab72b80f71e4d7b8e91540616eef1c319e0a439b
Kevin Grandon <kevingrandon@yahoo.com>
2017-11-09 10:52:47 -0800

    Simplify GH api usage with async functions.

--- src/utils/ensureGithubFork.mjs
+++ src/utils/ensureGithubFork.mjs
@@ -1,24 +1,22 @@
-function getRepo(api, repo) {
-  return new Promise((resolve, reject) => {
-    api.repos.get({
+async function getRepo(api, repo) {
+  try {
+    await api.repos.get({
       owner: process.env.GITHUB_USER,
       repo: repo.name
-    }).then((repo) => {
-      console.log(' - ok, found fork')
-      resolve();
-    }, (error) => {
-      console.log(' - repo not found, forking');
-      api.repos.fork({
-        owner: repo.upstream,
-        repo: repo.name,
-      }).then(
-        resolve,
-        () => {
-          console.warn(' - error forking repo');
-        }
-      );
     });
-  });
+    console.log(' - ok, found fork');
+  } catch(e) {
+    console.log(' - repo not found, forking');
+  }
+
+  try {
+    await api.repos.fork({
+      owner: repo.upstream,
+      repo: repo.name,
+    });
+  } catch(e) {
+    console.warn(' - error forking repo');
+  }
 }
 
 export default async function (api, repo) {
