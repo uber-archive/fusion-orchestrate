e6e443b9c780d4844825a8da6e1435bf53a2f069
Alexander Smith <amsmith@uber.com>
2017-11-27 17:20:53 -0800

    (minor) Unrestricted withEachRepo's repo list

--- src/utils/withEachRepo.js
+++ src/utils/withEachRepo.js
@@ -3,7 +3,7 @@ const getRepos = require('./../repos');
 
 module.exports = async function(eachCallback, parallel = false) {
   const repos = await getRepos();
-  for (let i = 0; i < 1 /*repos.length*/; i++) {
+  for (let i = 0; i < repos.length; i++) {
     const result = eachCallback(api, repos[i]);
     if (!parallel) {
       await result;
