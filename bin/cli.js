f8e13e568ec664cbda569fae387a67cb220080d9
Giancarlo Anemone <ganemone@uber.com>
2017-12-12 15:30:58 -0800

    Add options for --mine and --others

--- bin/cli.js
+++ bin/cli.js
@@ -10,6 +10,8 @@ const reviews = require('../src/reviews');
 args
   .command('setup', 'Fork and clone all the fusionjs repos', setup)
   .command('mergeAccepted', 'Merge all your accepted PRs', mergeAccepted)
+  .option('mine', 'Show only your pull requests')
+  .option('others', 'Show only others pull requests')
   .command('reviews', 'List all PRs needing review', reviews);
 
 args.parse(process.argv);
--- src/reviews.js
+++ src/reviews.js
@@ -3,9 +3,9 @@ const chalk = require('chalk');
 const logPR = require('./utils/logPR');
 const getOpenReviews = require('./utils/getOpenReviews');
 
-module.exports = async () => {
+module.exports = async (name, sub, options) => {
   let needsReview = await getOpenReviews();
-  const {yours, others} = needsReview.reduce(
+  let {yours, others} = needsReview.reduce(
     (byType, current) => {
       if (current.user.login === process.env.GITHUB_USER) {
         byType.yours.push(current);
@@ -16,8 +16,20 @@ module.exports = async () => {
     },
     {yours: [], others: []}
   );
-  console.log(chalk.bold.underline.red('\nNeeds your review:\n'));
-  others.forEach(logPR);
-  console.log(chalk.bold.underline.red('\nNeeds others review\n'));
-  yours.forEach(logPR);
+  if (options.mine) {
+    others = [];
+  } else if (options.others) {
+    yours = [];
+  }
+  if (others.length) {
+    console.log(chalk.bold.underline.red('\nNeeds your review:\n'));
+    others.forEach(logPR);
+  }
+  if (yours.length) {
+    console.log(chalk.bold.underline.red('\nNeeds others review\n'));
+    yours.forEach(logPR);
+  }
+  if (!others.length && !yours.length) {
+    console.log(chalk.bold.underline.red('\nCould not find any reviews\n'));
+  }
 };
