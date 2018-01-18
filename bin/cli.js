870d971dfdb4bbc252c6ebfe355531659a128bf7
Kevin Grandon <kevingrandon@yahoo.com>
2018-01-17 16:14:35 -0800

    Add command to enforce repository settings
    https://github.com/uber-workflow/fusion-orchestrate/pull/28

--- bin/cli.js
+++ bin/cli.js
@@ -4,12 +4,18 @@ process.on('unhandledRejection', e => {
 });
 const args = require('args');
 const setup = require('../src/setup');
+const enforceSettings = require('../src/enforce-settings');
 const mergeAccepted = require('../src/merge-accepted');
 const mergeRenovate = require('../src/merge-renovate-prs');
 const reviews = require('../src/reviews');
 
 args
   .command('setup', 'Fork and clone all the fusionjs repos', setup)
+  .command(
+    'enforceSettings',
+    'Enforces Github settings across all repositories',
+    enforceSettings
+  )
   .command('mergeAccepted', 'Merge all your accepted PRs', mergeAccepted)
   .command('mergeRenovate', 'Merge all your Renovate PRs', mergeRenovate)
   .option('mine', 'Show only your pull requests')
